5deb153aa37bd8f5716e4fa165065fe3af3cf555
Alfred Razol <alfred.razol@rte-france.com>
2026-03-24 16:16:12 +0100

    wip: change models
    Signed-off-by: Alfred Razol <alfred.razol@rte-france.com>

--- pypowsybl2grid/models.py
+++ pypowsybl2grid/models.py
@@ -27,9 +27,13 @@ class PhaseTapChangerUpdatePayload(BaseModel):
     updates: list[PhaseTapChangerUpdate]
 
     def to_df(self) -> pd.DataFrame:
-        return pd.DataFrame(
+        df = pd.DataFrame(
             [u.model_dump(exclude_none=True) for u in self.updates]
         ).set_index("id")
+        if "regulating" in df.columns:
+                cols = [c for c in df.columns if c != "regulating"] + ["regulating"]
+                df = df[cols]
+        return df
 
 
 class RatioTapChangerUpdatePayload(BaseModel):
